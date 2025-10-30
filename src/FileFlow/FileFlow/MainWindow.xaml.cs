b3c51a6f5aa8ab9879c433b7c50fa3b83e792401
3n <sjcup97@163.com>
2025-10-30 23:41:01 +0800

    feat:界面初始设计

--- src/FileFlow/FileFlow/MainWindow.xaml.cs
+++ src/FileFlow/FileFlow/MainWindow.xaml.cs
@@ -16,9 +16,75 @@ namespace FileFlow
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region 私有变量
+
+        
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region 私有方法
+
+        private void BrowseRootFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // 实现选择文件夹逻辑
+        }
+
+        private void SaveConfig_Click(object sender, RoutedEventArgs e)
+        {
+            // 保存配置逻辑
+        }
+
+        private void InstallContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // 注册右键菜单逻辑
+        }
+
+        private void UninstallContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+            // 卸载右键菜单逻辑
+        }
+
+        /// <summary>
+        /// 鼠标点击，系统级拖拽效果
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                // 直接触发系统级拖拽
+                DragMove();
+            }
+        }
+
+        /// <summary>
+        /// 最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 最小化窗口
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 关闭窗口
+            this.Close();
+        }
+
+        #endregion
     }
 }
---
+++ src/FileFlow/FileFlow/Model/Poco/StateFolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace FileFlow.Model.Poco
+{
+    public class StateFolder
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public SolidColorBrush Color { get; set; }
+
+        public StateFolder(string name, string path, SolidColorBrush color)
+        {
+            Name = name;
+            Path = path;
+            Color = color;
+        }
+    }
+}
